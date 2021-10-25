3522b24ef278bc4db77abee4b63c5261056b5efc
JuanEstebanRestrepo <juaerestrepovel@unal.edu.co>
2021-10-25 16:08:49 -0500

    improve-test

--- test/google.spec.ts
+++ test/google.spec.ts
@@ -1,9 +1,13 @@
 import { browser } from 'protractor';
 
-describe('This is the first example of protractor', () => {
-  it('should have a title', () => {
-    browser.ignoreSynchronization = true;
-    browser.get('http://www.google.com');
-    expect(browser.getTitle()).toEqual('Google');
+describe('Given a SDET learning protractor', () => {
+  describe('when open Google Page', () => {
+    beforeEach(() => {
+      browser.get('http://www.google.com');
+    });
+
+    it('then should have a title', () => {
+      expect(browser.getTitle()).toEqual('Google');
+    });
   });
 });
