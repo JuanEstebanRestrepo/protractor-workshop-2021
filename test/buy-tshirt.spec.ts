b48bb51720041ccf1ef67b56183c54cacceffb2f
JuanEstebanRestrepo <juaerestrepovel@unal.edu.co>
2021-10-26 20:06:43 -0500

    Separar en diferentes describes

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -3,7 +3,7 @@ import { MenuContentPage, ProductListPage, ProductAddedModalPage, SummaryStepPag
           SignInStepPage, AddressStepPage, ShippingStepPage, PaymentStepPage,
           BankPaymentPage, OrderSummaryPage } from '../src/page';
 
-describe('Buy a t-shirt', () => {
+/*describe('Buy a t-shirt', () => {
   const menuContentPage: MenuContentPage = new MenuContentPage();
   const productListPage: ProductListPage = new ProductListPage();
   const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
@@ -35,5 +35,60 @@ describe('Buy a t-shirt', () => {
 
     await expect($('#center_column > div > p > strong').getText())
       .toBe('Your order on My Store is complete.');
+  });
+});*/
+
+describe('Abrir la página en el navegador', () => {
+
+  it('open page', async () => {
+    await browser.get('http://automationpractice.com/');
+  });
+});
+
+describe('Proceso de compra de la Camiseta', () => {
+  const menuContentPage: MenuContentPage = new MenuContentPage();
+  const productListPage: ProductListPage = new ProductListPage();
+  const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
+  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
+
+  it('selected t-shirt', async () => {
+    await menuContentPage.goToTShirtMenu();
+    await productListPage.goToTShirtProductList();
+    await productAddedModalPage.goToTShirtProductAddedModal();
+    await summaryStepPage.goToTShirtSummaryStep();
+  });
+});
+
+describe('Logeo en la aplicación', () => {
+  const signInStepPage: SignInStepPage = new SignInStepPage();
+
+  it('logged in', async () => {
+    await signInStepPage.goToTShirtSignInStep();
+  });
+});
+
+describe('Seleccionar la dirección por defecto', () => {
+  const addressStepPage: AddressStepPage = new AddressStepPage();
+  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+
+  it('selected address', async () => {
+    await addressStepPage.goToTShirtAddressStep();
+    await shippingStepPage.goToTShirtShippingStep();
+  });
+});
+
+describe('Pago en el banco', () => {
+  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+
+
+  it('paid in the bank', async () => {
+    await paymentStepPage.goToTShirtPaymentStep();
+    await bankPaymentPage.goToTShirtBankPayment();
+    await orderSummaryPage.goToTShirtOrderSummary();
+
+    await expect($('#block_top_menu > ul > li:nth-child(3)').getText())
+      .toBe('T-SHIRTS');
   });
 });
