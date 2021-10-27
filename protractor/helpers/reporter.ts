92293dda3fb62ddce3d3f8e7b4cf9dddc9de3c31
JuanEstebanRestrepo <juaerestrepovel@unal.edu.co>
2021-10-26 20:59:38 -0500

    Jasmine Awesome

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,8 +1,17 @@
 import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
+
+const { AwesomeReport } = require('jasmine-awesome-report');
+
 export let reporter = () => {
+  const config = {
+    fullPath: 'reports',
+    fileName: 'awesome',
+    merge: true
+  };
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
       displayStacktrace: StacktraceOption.PRETTY
     }
   }));
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
 };
